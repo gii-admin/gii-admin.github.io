1f83f1c08058c67410d09843f09639db2a1baff8
han hee song <hanheesong@Han-MBP-13.local>
2025-06-14 00:19:21 -0700

    Minor updates

---
+++ js/image_data.js
@@ -0,0 +1,20 @@
+const imageData = {
+  mentors: [
+    { name: "Alice Kim", role: "Lead Mentor", img: "img/mentor_alice.jpg" },
+    { name: "John Lee", role: "Tech Mentor", img: "img/mentor_john.jpg" }
+  ],
+  students: [
+    { name: "Emily Chen", role: "Student", img: "img/student_emily.jpg" },
+    { name: "David Park", role: "Student", img: "img/student_david.jpg" }
+  ],
+  news: [
+    { title: "Launch Day", img: "img/news1.jpg", link: "#", date: "2025-06-01" },
+    { title: "Midterm Workshop", img: "img/news2.jpg", link: "#", date: "2025-05-15" },
+    { title: "Final Showcase", img: "img/news3.jpg", link: "#", date: "2025-04-20" }
+  ],
+  activities: [
+    { img: "img/activity1.jpg", desc: "Team-building hike" },
+    { img: "img/activity2.jpg", desc: "Robotics demo day" },
+    { img: "img/activity3.jpg", desc: "Hackathon night" }
+  ]
+};
